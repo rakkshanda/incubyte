3717686d2dc6c5cc975079466aae1b1c18d138c4
Rakshanda <rakshanda@analyticsquad4.com>
2022-03-14 05:56:35 +0530

    new lines between numbers

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-function sum(string) {
-    let stringSpilt = string.split(",");
-    const numbers= stringSpilt.map(Number);
+
+    function newLine(string){
+        let stringSpilt = string.split( /,|\n/);
+        const numbers= stringSpilt.map(Number);
+        return numbers;
     
+      }
+    
+  function add(string){
+return sum(newLine(string));
+  }
+  function sum(numbers) {    
     return numbers.reduce((prev, cur) => prev + cur);
   }
-console.log(sum("1,3,8"))
-document.getElementById("1").innerHTML = sum("1,3,8");
+
+
+      
+console.log(add("1,3,8"))
+document.getElementById("1").innerHTML = add("1\n 3");
